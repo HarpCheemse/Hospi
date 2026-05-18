1f6120dac3b59188f1145e0fb88db19939114440
HarpCheemse <sloshyteacher@gmail.com>
2026-05-18 12:42:38 +0700

    chore: add constants

---
+++ src/java/constant/Views.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constant;
+
+/**
+ *
+ * @author HarpCheemse
+ */
+public class Views {
+    public static final String HOME = "index";
+}
