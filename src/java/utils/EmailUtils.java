9effc2048f1478d93aa993e039f243a9dfbc2594
HarpCheemse <sloshyteacher@gmail.com>
2026-05-21 12:50:43 +0700

    chore: add email utils

---
+++ src/java/utils/EmailUtils.java
@@ -0,0 +1,61 @@
+package utils;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.Properties;
+
+public final class EmailUtils {
+
+    private static final String HOST = "smtp.gmail.com";
+    private static final String PORT = "587";
+    private static final String FROM;
+    private static final String PASSWORD;
+
+    static {
+        Properties config = new Properties();
+        try {
+            config.load(EmailUtils.class
+                    .getClassLoader()
+                    .getResourceAsStream("config/config.properties"));
+            FROM = config.getProperty("mail.username");
+            PASSWORD = config.getProperty("mail.password");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load mail config", e);
+        }
+    }
+
+    private EmailUtils() {
+    }
+
+    public static void send(String toEmail, String subject, String body)
+            throws MessagingException {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", HOST);
+        props.put("mail.smtp.port", PORT);
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(FROM, PASSWORD);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(FROM));
+        message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(toEmail));
+        message.setSubject(subject);
+        message.setText(body);
+
+        Transport.send(message);
+    }
+}
