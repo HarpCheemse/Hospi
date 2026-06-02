c645a309e3134d44f4d3d7d1cb97b1a7e03817b3
HarpCheemse <sloshyteacher@gmail.com>
2026-06-03 00:19:03 +0700

    chore: add extension to ResourceNotFoundException

--- src/main/java/com/hospi/manage/common/exception/ResourceNotFoundException.java
+++ src/main/java/com/hospi/manage/common/exception/ResourceNotFoundException.java
@@ -1,7 +1,12 @@
 package com.hospi.manage.common.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
+    private final String resource;
+
+    public ResourceNotFoundException(String resource) {
+        super(resource + " not found");
+        this.resource = resource;
     }
+
+    public String getResource() { return resource; }
 }
