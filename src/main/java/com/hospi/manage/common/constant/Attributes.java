7aed1eac126650daab81555feb1ae0cd54cf2b19
HarpCheemse <sloshyteacher@gmail.com>
2026-06-03 00:18:32 +0700

    chore: add success and error attributes

--- src/main/java/com/hospi/manage/common/constant/Attributes.java
+++ src/main/java/com/hospi/manage/common/constant/Attributes.java
@@ -4,4 +4,9 @@ public class Attributes {
     public static final String ACTIVE_SIDEBAR = "activeSidebar";
 
     public static final String FORM = "form";
+
+    public static final String SUCCESS = "success";
+    public static final String ERROR = "error";
+
+
 }
