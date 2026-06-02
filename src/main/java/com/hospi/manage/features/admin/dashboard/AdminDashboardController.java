795e82bc1eb68c660602f422c799dde8307d22b1
HarpCheemse <sloshyteacher@gmail.com>
2026-06-03 01:01:54 +0700

    feat: add admin dashboard controller

---
+++ src/main/java/com/hospi/manage/features/admin/dashboard/AdminDashboardController.java
@@ -0,0 +1,17 @@
+package com.hospi.manage.features.admin.dashboard;
+
+import com.hospi.manage.common.constant.Attributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminDashboardController {
+    @GetMapping
+    public String dashboard(Model model) {
+        model.addAttribute(Attributes.FORM, "DASHBOARD");
+        return "admin/dashboard";
+    }
+}
