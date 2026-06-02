6e84f8a733e5b1e0a279d1daa7199453be511598
HarpCheemse <sloshyteacher@gmail.com>
2026-06-03 00:39:56 +0700

    test: add testing to RoomValidator

---
+++ src/test/java/com/hospi/manage/features/manager/room/validator/RoomValidatorTest.java
@@ -0,0 +1,84 @@
+package com.hospi.manage.features.manager.room.validator;
+
+import com.hospi.manage.features.manager.room.dto.RoomCreateForm;
+import com.hospi.manage.features.manager.room.dto.RoomEditForm;
+import com.hospi.manage.features.manager.room.entity.Room;
+import com.hospi.manage.features.manager.room.enums.ConditionStatus;
+import com.hospi.manage.features.manager.room.repository.RoomRepository;
+import com.hospi.manage.features.manager.room.validation.RoomValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RoomValidatorTest {
+
+    @Mock
+    private RoomRepository roomRepository;
+
+    @InjectMocks
+    private RoomValidator roomValidator;
+
+    @Test
+    void shouldRejectWhenRoomNumberDoesNotMatchFloor() {
+
+        Room room = new Room();
+        room.setFloorNumber((short) 2);
+        room.setRoomNumber("201");
+
+        RoomEditForm form =
+                new RoomEditForm("101", 1L, ConditionStatus.CLEAN);
+
+        BindingResult errors =
+                new BeanPropertyBindingResult(form, "form");
+
+        roomValidator.validateUpdate(room, form, errors);
+
+        assertTrue(errors.hasFieldErrors("roomNumber"));
+    }
+
+    @Test
+    void shouldRejectWhenRoomNumberAlreadyExists() {
+
+        Room room = new Room();
+        room.setRoomNumber("102");
+        room.setFloorNumber((short) 1);
+
+        RoomEditForm form =
+                new RoomEditForm("101", 1L, ConditionStatus.CLEAN);
+
+        when(roomRepository.existsByRoomNumber("101"))
+                .thenReturn(true);
+
+        BindingResult errors =
+                new BeanPropertyBindingResult(form, "form");
+
+        roomValidator.validateUpdate(room, form, errors);
+
+        assertTrue(errors.hasFieldErrors("roomNumber"));
+    }
+
+    @Test
+    void shouldRejectWhenFloorCapacityExceeded() {
+
+        when(roomRepository.findHighestRoomNumberByFloor((short) 1))
+                .thenReturn(198);
+
+        RoomCreateForm form =
+                new RoomCreateForm((short) 1, 3, 1L);
+
+        BindingResult errors =
+                new BeanPropertyBindingResult(form, "form");
+
+        roomValidator.validateCreate(form, errors);
+
+        assertTrue(errors.hasFieldErrors("numberOfRooms"));
+    }
+}
