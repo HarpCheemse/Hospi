9b671f9142e03c551f539eea344040c749943cf2
HarpCheemse <sloshyteacher@gmail.com>
2026-06-05 02:34:29 +0700

    feat: add account entity

---
+++ src/main/java/com/hospi/manage/features/admin/account/entity/Account.java
@@ -0,0 +1,51 @@
+package com.hospi.manage.features.admin.account.entity;
+
+import com.hospi.manage.features.admin.account.enums.AccountStatus;
+import com.hospi.manage.features.admin.account.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "staffs")
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hotel_id")
+    private Long hotelId;
+
+    @Column(name = "full_name", length = 100)
+    private String fullName;
+
+    @Column(name = "email", unique = true, length = 255)
+    private String email;
+
+    @Column(name = "password_hash", length = 255)
+    private String passwordHash;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", length = 50)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", length = 50)
+    private AccountStatus status;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+}
