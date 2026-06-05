59933ab72b846318973d687e0eb8c87b9a9eea0b
HarpCheemse <sloshyteacher@gmail.com>
2026-06-05 15:21:29 +0700

    feat: add update account service

--- src/main/java/com/hospi/manage/features/admin/account/service/AccountService.java
+++ src/main/java/com/hospi/manage/features/admin/account/service/AccountService.java
@@ -1,8 +1,10 @@
 package com.hospi.manage.features.admin.account.service;
 
 import com.hospi.manage.common.constant.HotelConstants;
+import com.hospi.manage.common.exception.ResourceNotFoundException;
 import com.hospi.manage.core.security.Argon2HashingService;
 import com.hospi.manage.features.admin.account.dto.AccountCreateForm;
+import com.hospi.manage.features.admin.account.dto.AccountEditForm;
 import com.hospi.manage.features.admin.account.dto.AccountView;
 import com.hospi.manage.features.admin.account.entity.Account;
 import com.hospi.manage.features.admin.account.enums.AccountStatus;
@@ -21,6 +23,12 @@ public class AccountService {
                    Argon2HashingService argon2HashingService) {
         this.argon2HashingService = argon2HashingService;
         this.accountRepository = accountRepository;
+    }
+
+    public Account findById(Long id) {
+        return accountRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Account")
+        );
     }
 
     public List<AccountView> findAllViews() {
@@ -53,6 +61,47 @@ public class AccountService {
 
         account.setRole(form.role());
         account.setStatus(AccountStatus.DISABLED);
+
+        accountRepository.save(account);
+    }
+
+    public AccountView getAccountView(Long id) {
+        Account account = findById(id);
+        return new AccountView(
+                account.getId(),
+                account.getFullName(),
+                account.getEmail(),
+                account.getPhone(),
+                account.getRole(),
+                account.getStatus()
+        );
+    }
+
+    public AccountEditForm getEditForm(Long id) {
+        Account account = findById(id);
+
+        return new AccountEditForm(
+                account.getFullName(),
+                account.getEmail(),
+                account.getPhone(),
+                account.getRole(),
+                account.getStatus()
+        );
+    }
+
+    @Transactional
+    public void updateAccount(Long id, AccountEditForm form) {
+
+        Account account = accountRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Account not found with id: " + id)
+                );
+
+        account.setFullName(form.fullName());
+        account.setEmail(form.email());
+        account.setPhone(form.phone());
+        account.setRole(form.role());
+        account.setStatus(form.status());
 
         accountRepository.save(account);
     }
