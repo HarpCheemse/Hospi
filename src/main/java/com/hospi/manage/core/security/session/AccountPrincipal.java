f67742d0cdebac13bcae04b1a3b41152afc7e155
HarpCheemse <sloshyteacher@gmail.com>
2026-06-06 01:08:00 +0700

    feat: add AccountPrincipal

---
+++ src/main/java/com/hospi/manage/core/security/session/AccountPrincipal.java
@@ -0,0 +1,59 @@
+package com.hospi.manage.core.security.session;
+
+import com.hospi.manage.features.admin.account.entity.Account;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AccountPrincipal implements UserDetails {
+
+    private final Account account;
+
+    public AccountPrincipal(Account account) {
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + account.getRole().name())
+        );
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return account.getStatus().name().equals("ACTIVE");
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return account.getStatus().name().equals("ACTIVE");
+    }
+}
