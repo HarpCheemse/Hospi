ac449f871856fe908bb2ce574cbb96c5c9c82fe1
HarpCheemse <sloshyteacher@gmail.com>
2026-06-12 14:59:36 +0700

    feat: add payment service and payment repo

---
+++ src/main/java/com/hospi/manage/features/payment/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.hospi.manage.features.payment.repository;
+
+import com.hospi.manage.features.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByReservationId(Long reservationId);
+
+    boolean existsByReservationId(Long reservationId);
+}
