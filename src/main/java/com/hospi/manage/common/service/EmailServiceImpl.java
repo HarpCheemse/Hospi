90abbe99b70e29f687d894865af6f29dda4eed97
HarpCheemse <sloshyteacher@gmail.com>
2026-06-14 23:59:53 +0700

    chore: add console mode for email service

--- src/main/java/com/hospi/manage/common/service/EmailServiceImpl.java
+++ src/main/java/com/hospi/manage/common/service/EmailServiceImpl.java
@@ -2,11 +2,13 @@ package com.hospi.manage.common.service;
 
 import com.hospi.manage.common.interfaces.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
@@ -16,20 +18,47 @@ public class EmailServiceImpl implements EmailService {
     @Value("${spring.mail.username}")
     private String from;
 
+    @Value("${app.mail.console:false}")
+    private boolean consoleMode;
+
     @Override
-    public void send(
-            String to,
-            String subject,
-            String content) {
+    public void send(String to, String subject, String content) {
+        if (consoleMode) {
+            viaConsole(to,
+                    subject,
+                    content);
+            return;
+        }
+        viaEmail(to,
+                subject,
+                content);
+    }
 
-        SimpleMailMessage message =
-                new SimpleMailMessage();
-
+    private void viaEmail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(content);
 
         mailSender.send(message);
+    }
+
+    private void viaConsole(String to, String subject, String content) {
+        log.info("""
+                        
+                        ===== EMAIL =====
+                        To: {}
+                        Subject: {}
+                        
+                        {}
+                        
+                        =================
+                        """,
+                to,
+                subject,
+                content);
+
+        return;
     }
 }
