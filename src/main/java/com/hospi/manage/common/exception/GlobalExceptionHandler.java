940f82f432e5039377f04f01f2a42cd6ac529a73
HarpCheemse <sloshyteacher@gmail.com>
2026-06-22 13:31:11 +0700

    feat: add Exeption Handler to Global Exception Handler

--- src/main/java/com/hospi/manage/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hospi/manage/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.hospi.manage.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import static com.hospi.manage.common.constant.Attributes.ERROR;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -11,5 +16,21 @@ public class GlobalExceptionHandler {
         model.addAttribute("resource",
                 ex.getResource());
         return "error/404";
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public String handleIllegalState(IllegalStateException ex, RedirectAttributes redirect) {
+        log.warn("Illegal state: {}", ex.getMessage());
+        redirect.addFlashAttribute(ERROR,
+                ex.getMessage());
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handleIllegalArgument(IllegalArgumentException ex, RedirectAttributes redirect) {
+        log.warn("Illegal argument: {}", ex.getMessage());
+        redirect.addFlashAttribute(ERROR,
+                ex.getMessage());
+        return "redirect:/";
     }
 }
