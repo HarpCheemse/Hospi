89ba683c437e7707125ed9e884c1a30b41578bd9
HarpCheemse <sloshyteacher@gmail.com>
2026-06-23 18:35:20 +0700

    feat: add async email sending with @Async and thread pool

--- src/main/java/com/hospi/manage/common/service/EmailServiceImpl.java
+++ src/main/java/com/hospi/manage/common/service/EmailServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -22,6 +23,7 @@ public class EmailServiceImpl implements EmailService {
     private boolean consoleMode;
 
     @Override
+    @Async("emailTaskExecutor")
     public void send(String to, String subject, String content) {
         if (consoleMode) {
             viaConsole(to,
