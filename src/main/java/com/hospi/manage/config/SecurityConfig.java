9e5ddf20da5710809315f3481dd3eb8af600ecdb
dookubt <dookubt@niggabite.local>
2026-06-30 11:54:03 +0700

    config: secure leader routes to LEADER role

--- src/main/java/com/hospi/manage/config/SecurityConfig.java
+++ src/main/java/com/hospi/manage/config/SecurityConfig.java
@@ -100,6 +100,7 @@ public class SecurityConfig {
                         .requestMatchers("/manager/**").hasRole("MANAGER")
                         .requestMatchers("/receptionist/**").hasAnyRole("RECEPTIONIST",
                                 "ADMIN")
+                        .requestMatchers("/leader/**").hasRole("LEADER")
 
                         .anyRequest().authenticated())
 
